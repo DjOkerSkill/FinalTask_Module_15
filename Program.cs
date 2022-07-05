cacdfb1756f4ec7cd278665fdbe2a4bf18a92736
Роман <ram23@mail.ru>
2022-07-05 14:58:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace FinalTask_Module_15
+{
+    /// <summary>
+    /// Список студентов в одном классе
+    /// </summary>
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+            
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        /// <summary>
+        /// Добавляет учихся из всех классов в один список
+        /// </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = (from c in classes
+                              from s in c.Students
+                              select s).ToArray();
+            return allStudents;
+        }
+
+
+    }
+    
+}
